a6af47f6ef090475a2c050d1cf255d7ce582d479
Daniel Prejs <danielnprejs@gmail.com>
2021-12-01 10:07:26 -0700

    aoc day one part one

---
+++ aoc1/aoc1.js
@@ -0,0 +1,11 @@
+let list =  require('./aoc1Data.js');
+list = list.split('\n');
+list = list.map((x) => parseInt(x))
+let count = 0;
+for (let i = 1; i < list.length; i++) {
+
+  if (list[i] > list[i-1]) {
+    count ++;
+  }
+}
+console.log(count);
